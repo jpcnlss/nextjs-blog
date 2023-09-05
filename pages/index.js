75f3aa5dbec7f7b40a88419e93dfb7a5fed1f9e7
jpcnlss <123640714+jpcnlss@users.noreply.github.com>
2023-09-05 14:53:00 +0800

    index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, my name is Jaypee Canlas, and I'm from San Isidro Minalin, Pampanga. 
-		My hobbies are playing mobile games, watching anime, and experimenting with gadgets—even if sometimes 
-		I wind up destroying them much more than I intended to.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>During my internship at TwistResources Inc., I had the opportunity to immerse myself in the dynamic world of networking and troubleshooting. 
+	  Each day was a valuable learning experience as I delved into the intricacies of managing and optimizing network systems. Not only did I gain 
+	  hands-on experience in these crucial areas, but the team at TwistResources also generously imparted their knowledge on deploying in AWS, expanding 
+		  my skill set significantly. Furthermore, the internship provided a fantastic platform for networking, both in terms of forging new friendships 
+			  with fellow interns and professionals within the company. These connections have not only enriched my professional network 
+				  but also added a personal dimension to my internship, making it an unforgettable experience.</p>
+        
       </section>
     </Layout>
   );
