bbb02164fae4f08fabdf118def61801c610ed5c8
jpcnlss <123640714+jpcnlss@users.noreply.github.com>
2023-09-14 13:59:27 +0800

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,29 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p><br>10 Things That Require Zero Talent</br>
+        <p>
+    <br>10 Things That Require Zero Talent</br>
 
 <br>1. Being On Time</br>
+    
 <br>2. Making An Effort</br>
+    
 <br>3. Being High Energy</br>
+    
 <br>4. Having A Positive Attitude</br>
+    
 <br>5. Being Passionate</br>
+
 <br>6. Using Good Body Language</br>
+    
 <br>7. Being Coachable</br>
+    
 <br>8. Doing A Little Extra</br>
+    
 <br>9. Being Prepared</br>
-<br>10. Having A Strong Work Ethic</br></p>
+    
+<br>10. Having A Strong Work Ethic</br>
+    </p>
         
       </section>
     </Layout>
